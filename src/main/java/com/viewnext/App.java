e22252d21f9f315a31d704788e735dea0925ada3
Juan Jesús Martin <juanjesus.martin@viewnext.com>
2023-02-21 11:12:22 +0100

    commit inicial

---
+++ src/main/java/com/viewnext/App.java
@@ -0,0 +1,14 @@
+package com.viewnext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       Vista vista= new Vista();
+       vista.setVisible(true);
+    }
+}
